4fdec6d008e1498b6c0c449b695b7e7f57328ddc
sarthak9365 <60870823+sarthak9365@users.noreply.github.com>
2021-05-18 23:04:32 +0530

    Changes in Auth
    Main Activity created for getting phonenumber and country data class created to store all country names and country codes

--- app/src/main/java/com/example/things/Auth/MainActivity.java
+++ app/src/main/java/com/example/things/Auth/MainActivity.java
@@ -2,19 +2,56 @@ package com.example.things.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.things.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Spinner spinner_country;
+    private EditText editText_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner_country = findViewById(R.id.spinnerCountries);
 
+        //set All countryNames in Spinner
+        spinner_country.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,CountryData.countryNames));
+
+        editText_number = findViewById(R.id.editTextPhone);
+
+        editText_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String code = CountryData.countryAreaCodes[spinner_country.getSelectedItemPosition()];
+
+                String number = editText_number.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editText_number.setError("Valid number is required");
+                    editText_number.requestFocus();
+                    return;
+                }
+
+                String phoneNumber = "+" + code + number;
+
+            }
+        });
 
 
     }
+
+   
+
 }
