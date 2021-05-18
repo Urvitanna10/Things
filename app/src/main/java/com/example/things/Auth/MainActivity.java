8e827464897a94fd11782faba6c091868abab1e2
sarthak9365 <60870823+sarthak9365@users.noreply.github.com>
2021-05-18 22:22:43 +0530

    Auth Folder Created
    Auth folder created for authentication purpose

--- app/src/main/java/com/example/things/Auth/MainActivity.java
+++ app/src/main/java/com/example/things/Auth/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.things;
+package com.example.things.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.things.R;
 
 public class MainActivity extends AppCompatActivity {
 
