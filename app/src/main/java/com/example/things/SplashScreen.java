c23d6c69e1409e5cfb908a06f4e9888a9f70a1ac
sarthak9365 <60870823+sarthak9365@users.noreply.github.com>
2021-05-18 20:58:29 +0530

    Layout change
    Splash screen layout added .

--- app/src/main/java/com/example/things/SplashScreen.java
+++ app/src/main/java/com/example/things/SplashScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,9 +12,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent i = new Intent(SplashScreen.this,MainActivity.class);
-        startActivity(i);
-        finish();
+        new Handler().postDelayed(new Runnable() {
 
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
+
 }
