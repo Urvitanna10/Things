bc45d12675ce639422c34366c4de3f2d91866315
Urvitanna10 <tannaurvi10@gmail.com>
2021-05-19 00:01:39 +0530

    verify phone number

--- app/src/main/java/com/example/things/Auth/MainActivity.java
+++ app/src/main/java/com/example/things/Auth/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner_country;
     private EditText editText_number;
+    private Button button_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +31,16 @@ public class MainActivity extends AppCompatActivity {
         spinner_country.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,CountryData.countryNames));
 
         editText_number = findViewById(R.id.editTextPhone);
+        button_continue = findViewById(R.id.buttonContinue);
 
-        editText_number.setOnClickListener(new View.OnClickListener() {
+        button_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String code = CountryData.countryAreaCodes[spinner_country.getSelectedItemPosition()];
 
                 String number = editText_number.getText().toString().trim();
+
 
                 if (number.isEmpty() || number.length() < 10) {
                     editText_number.setError("Valid number is required");
@@ -45,13 +49,26 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 String phoneNumber = "+" + code + number;
+                Intent intent = new Intent(MainActivity.this, VerifyPhoneActivity.class);
+                intent.putExtra("phonenumber", phoneNumber);
+                startActivity(intent);
 
             }
         });
 
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
+    }
    
 
 }
